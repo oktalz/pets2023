ac41acf97dfe9ce8a69211fe33bd7bc17a34a858
Zlatko Bratkovic <zbratkovic@haproxy.com>
2023-05-04 19:52:21 +0200

    MAJOR: v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/oktalz/pets2023
+module github.com/oktalz/pets2023/v2
 
 go 1.20
--- pets.go
+++ pets.go
@@ -5,6 +5,6 @@ import "math/rand"
 var prefix = [...]string{"big", "small", "tiny", "angry"}
 var pet = [...]string{"cat", "dog", "fish", "elephant"}
 
-func PetName() string {
-	return prefix[rand.Intn(len(prefix))] + "-" + pet[rand.Intn(len(pet))]
+func PetName(separator string) string {
+	return prefix[rand.Intn(len(prefix))] + separator + pet[rand.Intn(len(pet))]
 }
